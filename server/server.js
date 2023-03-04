c52d90b879fd33f045bc46778ebbdfe99529a4f4
Akash Gupta <akashgp9@gmail.com>
2023-03-04 09:11:29 +0530

    add gitignore

--- server/server.js
+++ server/server.js
@@ -50,14 +50,13 @@ app.use(
 );
 
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("*", (req, res) => {
+  app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(__dirname, "../", "client", "build", "index.html")
-    );
-  });
+    )
+  );
 }
 // set up routes
 app.use("/contact", contactRouter);
